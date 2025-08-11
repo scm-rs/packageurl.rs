632a11864ce6538c5f036a00543d7c894f2499b2
Jens Reimann <ctron@dentrassi.de>
2025-08-11 10:11:14 +0200

    chore: make clippy 1.89.0 happy

--- src/utils/encodable.rs
+++ src/utils/encodable.rs
@@ -3,33 +3,33 @@ use percent_encoding::PercentDecode;
 use percent_encoding::PercentEncode;
 
 pub trait PercentCodec {
-    fn encode(&self, encode_set: &'static AsciiSet) -> PercentEncode;
-    fn decode(&self) -> PercentDecode;
+    fn encode(&self, encode_set: &'static AsciiSet) -> PercentEncode<'_>;
+    fn decode(&self) -> PercentDecode<'_>;
 }
 
 impl PercentCodec for [u8] {
-    fn encode(&self, encode_set: &'static AsciiSet) -> PercentEncode {
+    fn encode(&self, encode_set: &'static AsciiSet) -> PercentEncode<'_> {
         ::percent_encoding::percent_encode(self, encode_set)
     }
-    fn decode(&self) -> PercentDecode {
+    fn decode(&self) -> PercentDecode<'_> {
         ::percent_encoding::percent_decode(self)
     }
 }
 
 impl PercentCodec for str {
-    fn encode(&self, encode_set: &'static AsciiSet) -> PercentEncode {
+    fn encode(&self, encode_set: &'static AsciiSet) -> PercentEncode<'_> {
         self.as_bytes().encode(encode_set)
     }
-    fn decode(&self) -> PercentDecode {
+    fn decode(&self) -> PercentDecode<'_> {
         self.as_bytes().decode()
     }
 }
 
 impl PercentCodec for ::std::borrow::Cow<'_, str> {
-    fn encode<'s>(&self, encode_set: &'static AsciiSet) -> PercentEncode {
+    fn encode<'s>(&self, encode_set: &'static AsciiSet) -> PercentEncode<'_> {
         self.as_bytes().encode(encode_set)
     }
-    fn decode(&self) -> PercentDecode {
+    fn decode(&self) -> PercentDecode<'_> {
         self.as_bytes().decode()
     }
 }
